03710fe97171155b529cc2957b05c4e33067060c
Mathireddi Munikumar <munikumar3456@gmail.com>
2026-07-16 12:02:14 +0530

    frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  constructor(private router: Router) {}
+
+  register() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  registerWithGoogle() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  registerWithFacebook() {
+    this.router.navigate(['/dashboard']);
+  }
+
+}
